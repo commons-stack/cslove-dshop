8b232f510b89597fae9d04e5f2c4eb76c3ecbfe3
skylar745 <67788159+skylar745@users.noreply.github.com>
2021-02-15 16:14:31 +0100

    change price of CSLOVE

--- backend/routes/cs-price.js
+++ backend/routes/cs-price.js
@@ -2,6 +2,6 @@
 module.exports = function (router) {
   // This endpoint will tell the price of the CSLOVE token
   router.get('/cs/price', async (req, res) => {
-    res.json({ CSLOVE: 1 / 50 })
+    res.json({ CSLOVE: 1 / 55 })
   })
 }
